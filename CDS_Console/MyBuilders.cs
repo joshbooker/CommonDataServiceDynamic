67ae6e0b71626a2481ae1fef76eaa1a0f676b4a4
JoshuaABooker <JoshuaABooker@DESKTOP-R5ANG0N>
2017-07-26 10:40:13 -0400

    Add project files.

---
+++ CDS_Console/MyBuilders.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Microsoft.CommonDataService;
+using Microsoft.CommonDataService.Builders;
+using Microsoft.CommonDataService.Entities;
+
+using System.Linq.Dynamic;
+using System.Linq.Expressions;
+
+namespace Microsoft.CommonDataService
+{
+    public static class MyBuilders
+    {
+        /*public static RelationalEntitySet GetRelationalEntitySet(Client client, string @namespace, string name, Version version)
+        {
+            return client.GetRelationalEntitySet(@namespace, name, version);// Microsoft.CommonDataService.Entities.TypedRelationalEntitySet;
+        }
+        public static RelationalEntitySet<TEntity>  GetRelationalEntitySet<TEntity>(Client client) where TEntity : TypedRelationalEntity, new()
+        {
+            return client.GetRelationalEntitySet<TEntity>();// Microsoft.CommonDataService.Entities.TypedRelationalEntitySet;
+        }
+        public static FromClauseBuilder<TEntity>  MyCreateQueryBuilder<TEntity>(this RelationalEntitySet<TEntity> entitySet) where TEntity : RelationalEntity//, new()
+        {
+            return entitySet.CreateQueryBuilder<TEntity>();
+        }*/
+        public static Builders.WhereClauseBuilder<TEntity> Where<TEntity>(this Builders.FromClauseBuilder<TEntity> entitySet, string predicate) where TEntity : RelationalEntity//, new()
+        {
+            return entitySet.Where(WhereExpression<TEntity>(predicate));
+        }
+        public static Expression<Func<T,bool>> WhereExpression<T>(string predicate, params object[] values)
+        {
+            return System.Linq.Dynamic.DynamicExpression.ParseLambda<T, bool>(predicate, values);
+        }
+    }
+}
---
+++ CDS_Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.CommonDataService;
+using Microsoft.CommonDataService.CommonEntitySets;
+using Microsoft.CommonDataService.Configuration;
+using Microsoft.CommonDataService.ServiceClient.Security;
+using Microsoft.CommonDataService.Entities;
+
+using System.Linq.Dynamic;
+//using System.Linq.Expressions;
+
+namespace CDS_Console
+{
+
+    class Program
+    {   [STAThread] 
+        static void Main(string[] args)
+        {
+            using (var client = ConnectionSettings.Instance.CreateClient().Result)
+            {
+                //var query2 = client.GetRelationalEntitySet("Microsoft.CommonDataService.CommonEntities", "ProductCategory", Microsoft.CommonDataService.Version.Create("1.0.0"))
+                var query2 = client.GetRelationalEntitySet<ProductCategory>()
+                        .CreateQueryBuilder()
+                        //.Where(pc => pc.Name == "Surface" || pc.Name == "Phone")
+                        .Where("Name = \"Surface\" OR Name = \"Phone\"")
+                        .Project(pc => pc.SelectField(f => f["CategoryId"]).SelectField(f => f["Name"]));
+
+                OperationResult<IReadOnlyList<ProductCategory>> queryResult = null;
+                client.CreateRelationalBatchExecuter(RelationalBatchExecutionMode.Transactional)
+                     .Query(query2, out queryResult)
+                     .ExecuteAsync().Wait();
+
+                
+                 foreach (var entry in queryResult.Result)
+                 {
+                    Console.WriteLine(entry.Name);
+                 }
+            }
+        }
+    }
+}
